859459cec37693e02c6af27d69fd84566100e0ee
siyabongahenry <siyabongahenry23@gmail.com>
2026-01-04 22:06:50 +0200

    added multiple clockings route and updated column names

--- app/api/v1/routers/multiple_clockings_router.py
+++ app/api/v1/routers/multiple_clockings_router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter,UploadFile,File
 from app.utils.excel_upload_utils import load_excel_file
 from app.utils.export_utils import export_excel_and_get_url
 from pathlib import Path
+from app.services.multiple_clockings_service import MultipleClockigsService
 
 router = APIRouter(prefix="/multiple-clockings",tags=["multiple clockings report"])
 
@@ -21,5 +22,33 @@ async def multiple_clockings(file:UploadFile):
     # Ensures required columns exist before processing
     df = await load_excel_file(
         file,
-        required_columns={"Entry No.","Resource     no.", "VIP Code","Hours worked","Applies-To Entry"},
+        required_columns={"Clock No.","Date"},
     )
+
+    multiple_clockings_service = MultipleClockigsService(df)
+
+    multiple_clockings = multiple_clockings_service.getMultipleClockings()
+
+    # Return early if no issues are found
+    if multiple_clockings:
+        return {
+            "message": "Multiple clockings not found",
+            "incorrect_rows": 0,
+        }
+
+    # Export validation results to Excel and generate download URL
+    download_url = export_excel_and_get_url(
+        sheets={
+            "Multiple clockings": multiple_clockings,
+        },
+        prefix="multiple-clockings",
+        filename_prefix="multiple_clockings",
+    )
+
+    # Return summary and download link
+    return {
+        "multiple_clockings_count": len(multiple_clockings),
+        "download_url": download_url,
+    }
+
+
