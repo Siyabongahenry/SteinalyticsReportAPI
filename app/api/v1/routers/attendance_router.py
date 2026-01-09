76ae11184bb085b14e97d05009afde2ade720d34
siyabongahenry <siyabongahenry23@gmail.com>
2026-01-09 20:23:49 +0200

    created attendance router and added a router for list

---
+++ app/api/v1/routers/attendance_router.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter,Depends
+from app.services.attendence_service import AttendanceService
+from app.dependencies.file_upload_validator import FileUploadValidator
+from app.utils.excel_upload_utils import load_excel_file
+from app.utils.export_utils import export_excel_and_get_url
+
+router = APIRouter(prefix = "attendance",tags="Employees attendance")
+
+@router.post("/list")
+async def attendence_list(contents: bytes = Depends(FileUploadValidator())):
+
+    df = await load_excel_file(
+       contents,
+        required_columns={
+            "Clock No.",
+            "Date",
+            "WTT"
+        },
+     )
+    
+    attendence_service = AttendanceService(df)
+
+    attendence_list = attendence_service.get_employees_list()
+
+    download_url = export_excel_and_get_url(
+        sheets={
+            "Attendence_list":  attendence_list
+        },
+        prefix="Employees attence list",
+        filename_prefix="attendance_list",
+     )
+
+
+    return {
+        "download_url":download_url
+    }
