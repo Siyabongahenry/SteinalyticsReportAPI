8ebe8c0f8342a26607ec2d991cd5e57cc160349d
Siyabonga Vilakazi <102644493+Siyabongahenry@users.noreply.github.com>
2026-01-12 07:56:15 +0000

    Created allowance posted function - This function get the total number of entries posted per posting date

--- app/services/productivity_report_service.py
+++ app/services/productivity_report_service.py
@@ -7,10 +7,10 @@ class ProductivityReportService:
     def hours_worked_by_clerk(self):
 
         self.df_hours_worked["Work date"] = pd.to_datetime(self.df_hours_worked["Work date"])
-        
-        group = self.df_hours_worked.groupby(["Resource no.", "Work date"])["Hours worked"].sum().reset_index()
 
-        return group
+        df_clerk_hours = self.df_hours_worked[self.df_hours_worked["VIP Code"].isin([100,110,111,113,114,115,116,117,290,601,602,603,604,700,750,752,801,802,803,804])].copy()
+        
+        return df_clerk_hours.groupby(["Resource no.", "Work date"])["Hours worked"].sum().reset_index()
     
     def productive_hours_posted(self):
 
@@ -20,8 +20,13 @@ class ProductivityReportService:
 
         return posted_productive_df.groupby([ "User Originator", "Posting Date"])["Entry No."].count().reset_index()
 
+    def allowance_posted(self):
 
+         self.df_hours_posted["Posting Date"] = pd.to_datetime(self.df_hours_posted["Posting Date"])
 
+         df_posted_allowance = self.df_hours_posted[self.df_hours_posted["VIP Code"].isin[101] or self.df_hours_posted["VIP Code"] >= 900 ]
+        
+         return df_posted_allowance.groupby(["Entry No.","Posting Date"]).count().reset_index()
 
 
 
