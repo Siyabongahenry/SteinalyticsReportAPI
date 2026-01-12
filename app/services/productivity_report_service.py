025f6d898fe31563275af653735331fc2b368624
Siyabonga Vilakazi <102644493+Siyabongahenry@users.noreply.github.com>
2026-01-12 08:15:33 +0000

    Created get_summary function

--- app/services/productivity_report_service.py
+++ app/services/productivity_report_service.py
@@ -10,7 +10,7 @@ class ProductivityReportService:
 
         df_clerk_hours = self.df_hours_worked[self.df_hours_worked["VIP Code"].isin([100,110,111,113,114,115,116,117,290,601,602,603,604,700,750,752,801,802,803,804])].copy()
         
-        return df_clerk_hours.groupby(["Resource no.", "Work date"])["Hours worked"].sum().reset_index()
+        return df_clerk_hours.groupby(["Resource no.", "Work date"])["Hours worked"].sum().reset_index(name="Hours worked")
     
     def productive_hours_posted(self):
 
@@ -18,7 +18,7 @@ class ProductivityReportService:
 
         posted_productive_df = self.df_hours_posted[self.df_hours_posted["VIP Code"].isin([100,110,111,113,114,115,116,117,290,601,602,603,604,700,750,752,801,802,803,804])].copy()
 
-        return posted_productive_df.groupby([ "User Originator", "Posting Date"])["Entry No."].count().reset_index()
+        return posted_productive_df.groupby([ "User Originator", "Posting Date"])["Entry No."].count().reset_index(name="Entries posted")
 
     def allowance_posted(self):
 
@@ -26,7 +26,23 @@ class ProductivityReportService:
 
          df_posted_allowance = self.df_hours_posted[self.df_hours_posted["VIP Code"].isin[101] or self.df_hours_posted["VIP Code"] >= 900 ]
         
-         return df_posted_allowance.groupby(["Entry No.","Posting Date"]).count().reset_index()
+         return df_posted_allowance.groupby(["User Originator","Posting Date"]).count().reset_index(name="Entries posted")
 
 
+    def get_summary(self):
+
+        df_hours_worked = self.hours_worked_by_clerk()
+        df_allowance_entries_posted = self.allowance_posted()
+        df_productive_entries_posted = self.productive_hours_posted()
+
+        hours_worked_total = df_hours_worked.groupby(["User Originator"])["Hours worked"].sum().reset_index(name="Hours worked")
+
+        total_posted_concat = pd.concat([df_allowance_entries_posted,df_productive_entries_posted], ignore_index=True)
+
+        total_posted = total_posted_concat.groupby(["User Originator"])["Entries posted"].sum().reset_index(name="Entries posted")
+
+
+        summary_df = pd.merge(hours_worked_total,total_posted, on="User Originaror", how = "left")
+
+        return summary_df
 
