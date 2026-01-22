33707a5781b60707c4f87403d4321e0eed26e1b1
siyabongahenry <siyabongahenry23@gmail.com>
2026-01-22 21:55:51 +0200

    Removed time from date

--- app/services/device_service.py
+++ app/services/device_service.py
@@ -1,6 +1,10 @@
+import pandas as pd
 class DeviceService:
     def __init__(self, df):
-        self.df = df
+        self.df = df.copy()
+
+        # Ensure Date is a datetime object
+        self.df["Date"] = pd.to_datetime(self.df["Date"]).dt.date  # Keep only the date part
 
     def unique_clocks_per_meter_per_day(self):
         result = (
@@ -9,5 +13,4 @@ class DeviceService:
             .nunique()
             .reset_index(name="Unique_Clock_Count")
         )
-
         return result
