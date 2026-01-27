930ee1893d716c3b7ffec4c25fac470d929927e8
Siyabonga Vilakazi <102644493+Siyabongahenry@users.noreply.github.com>
2026-01-27 07:54:22 +0000

    Fixed token return - ro return decode_token

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -1,19 +1,17 @@
-import logging
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials
-
 from app.core.security import bearer_scheme
 from app.auth.tokens import decode_access_token
 from app.utils.exceptions import AuthenticationError
-logger = logging.getLogger("FastAPIApp")
 
 async def get_current_user(
     credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
 ):
     try:
-        logger.info(f"This is the user id: {credentials.credentials}")
-        
-        return await decode_access_token(credentials.credentials)
+
+        decoded_token = await decode_access_token(credentials.credentials)
+
+        return decoded_token
     except AuthenticationError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
