000e6db85c6577abdadc80e2d87b6ea1840256b3
Siyabonga Vilakazi <102644493+Siyabongahenry@users.noreply.github.com>
2026-02-18 07:52:49 +0000

    Loggin gemini models

--- app/core/generativeAIConfig.py
+++ app/core/generativeAIConfig.py
@@ -1,10 +1,14 @@
 from app.core.settings import settings
 import google.genai as genai
 
+
+
 class GoogleAIClient:
     def __init__(self):
         # Create a client with your API key from settings
         self.client = genai.Client(api_key=settings.google_api_key)
+        for model in self.client.models.list(): 
+            print(model.name, model.supported_generation_methods)
 
     def ask(self, prompt: str) -> str:
         # Use the client to generate content
