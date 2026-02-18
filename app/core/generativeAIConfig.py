856afb561d3ddd9955eee85e05e97a59d1489bab
Siyabonga Vilakazi <102644493+Siyabongahenry@users.noreply.github.com>
2026-02-18 07:37:42 +0000

    Update gen ai package

--- app/core/generativeAIConfig.py
+++ app/core/generativeAIConfig.py
@@ -1,12 +1,12 @@
-import google.generativeai as genai
 from app.core.settings import settings
+import google.genai as genai
 
 class GoogleAIClient:
     def __init__(self):
         # Configure API key once
         genai.configure(api_key=settings.google_api_key)
-        # Initialize the model once
-        self.model = genai.GenerativeModel("gemini-1.5-flash")
+        # Initialize a supported free model
+        self.model = genai.GenerativeModel("gemini-1.0-pro")
 
     def ask(self, prompt: str) -> str:
         response = self.model.generate_content(prompt)
